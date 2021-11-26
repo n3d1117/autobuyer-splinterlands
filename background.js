703293300cd27c8952f32cbe8ad83ea2766f6cde
ned <ned@me.com>
2021-11-26 19:24:02 +0100

    update

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
-chrome.runtime.onMessage.addListener(
-    function(message, _sender, _sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, _sender, _sendResponse) {
         console.log('message received from background')
         switch (message.type) {
             case 'acceptHiveTransaction':
@@ -11,23 +10,23 @@ chrome.runtime.onMessage.addListener(
 
 function acceptHiveTransaction() {
     console.log('accepting hive transaction...')
-    chrome.windows.getAll({ populate: true }, function(windows) {
+    chrome.windows.getAll({populate: true}, function (windows) {
         console.log(windows);
-        windows.forEach(function(window) {
+        windows.forEach(function (window) {
             if (window.type === 'popup') {
-                window.tabs.forEach(function(tab) {
+                window.tabs.forEach(function (tab) {
                     if (tab.title === 'Hive Keychain') {
                         console.log(tab);
                         console.log('attaching chrome debugger')
                         chrome.debugger.attach({
                             tabId: tab.id
-                        }, '1.0', function() {
+                        }, '1.0', function () {
                             console.log('sending command')
                             chrome.debugger.sendCommand({
                                 tabId: tab.id
                             }, "Runtime.evaluate", {
                                 expression: "document.getElementById('cancel').click();"
-                            }, function(response) {
+                            }, function (response) {
                                 console.log(response);
                             });
                         });
